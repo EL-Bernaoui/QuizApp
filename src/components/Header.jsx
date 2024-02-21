e05567b55ab7cb9c428ef52eab5f684b67e6465e
Achraf <achrafbase17@gmail.com>
2024-02-21 22:51:24 +0100

    Upload Files

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import headerImg from "../assets/quiz-logo.png";
+
+export default function Quiz() {
+  return (
+    <header>
+      <img src={headerImg} alt="Quiz Logo Image " />
+      <h1>ReactQuiz</h1>
+    </header>
+  );
+}
